c973456d22f8f7e750cc107723fc58515693a489
raphael2692 <rspataro@softstrategy.it>
2025-07-03 11:33:38 +0200

    add todo admin

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Todo
 
-# Register your models here.
+@admin.register(Todo)
+class TodoAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_by', 'created_at', 'assigned_to')
+    search_fields = ('title', 'description')
+    list_filter = ('created_by', 'assigned_to')
